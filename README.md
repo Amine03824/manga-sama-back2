63069722b3614595c27c4cfd3c8053ec7c165693
Amine03824 <amine.boukraa@protonmail.com>
2023-12-13 22:31:35 +0100

    🐞 fix(L'upload d'images et le routage des photos d'utilisateurs et d'annonces est fonctionnel):
    Les routes pour spécifier le chemin des photos d'articles et d'users étaient mal réglées ! Je me suis inspiré de ce que j'avais fait pour les covers juste au dessus, plus simple comme ça!

--- README.md
+++ README.md
@@ -1,4 +1,24 @@
-# Bienvenue sur le Repo Back du projet Manga-Sama 🍣
+# Bienvenue sur le Repo Back du projet Manga-Sama 🍣  
 
-## 🌿 Last branch standing : Setup Manga API + Database
+## 🌿 Last branch standing : feat/userUpload 📤
 
+## ~~🌿 Last branch standing : feat/TRANSACTION+STRIPE 💳~~
+
+## ~~🌿 Last branch standing : feat/userCreation+authentication 🔐~~
+
+## ~~🌿 Last branch standing : fix/SERVER_CONFIG 🛠️~~
+
+## ~~🌿 Last branch standing : feat/API-ORGANISATION 🚀~~
+
+## ~~🌿 Last branch standing : feat/associations 🤝~~
+
+##  ~~🌿 Last branch standing : Setup Manga API + Database 📚 + 🗄️~~
+
+## ~~🌿 Last branch standing : Feat/SERVER-SETUP 🛠️ + 🚀~~
+
+## ~~🌿 Last branch standing : Feat/architecture 🏰~~
+
+
+![ESint](https://s6.gifyu.com/images/S8wD4.gif "ESLint")
+
+![tonio sur son ordi](https://media.tenor.com/o5ewu4vTJ58AAAAd/old-man-computer.gif)
--- app/routes/images.js
+++ app/routes/images.js
@@ -2,30 +2,37 @@
 const express = require("express");
 const router = express.Router();
 // Import du controlleur
-const imageController = require('../controllers/imageController');
+const imageController = require("../controllers/imageController");
 // Import du middleware Multer
-const multerMiddleware = require('../middlewares/multer');
+const multerMiddleware = require("../middlewares/multer");
+
+// Import du middleware d'authentification
+const {
+  authenticateMiddleware
+} = require("../middlewares/authenticationMiddleware");
 
 // Routes des couvertures de mangas
-router.route('/:id')
-  .get(imageController.sendOneCoverById);
+router.route("/:id").get(imageController.sendOneCoverById);
 
-// Routes des images de profil mises en lignes par les utilisateurs
-router.route('/uploads/user/:id')
+//  Routes des images de profil mises en lignes par les utilisateurs
+router
+  .route("/uploads/user/:id")
   .get(imageController.sendOneUserImageById)
-  .post(multerMiddleware.uploadUser, imageController.uploadImageUser);
-
+  .post(
+    authenticateMiddleware,
+    multerMiddleware.uploadUser,
+    imageController.uploadImageUser
+  );
 
 // Routes des photos de mangas mises en lignes par les utilisateurs
-router.route('/uploads/article/:id')
+router
+  .route("/uploads/article/:id")
   .get(imageController.sendOneArticleImageById)
-  .post(multerMiddleware.uploadArticle, imageController.uploadImageArticle);
-
-
-// Route pour l'upload d'images
-router.route('/upload');
-
-
+  .post(
+    authenticateMiddleware,
+    multerMiddleware.uploadArticle,
+    imageController.uploadImageArticle
+  );
 
 // Export
 module.exports = router;
